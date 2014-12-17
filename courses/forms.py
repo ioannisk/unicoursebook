61ad0adda64cd02ff8d9975b3f8e7ef46ee85223
Ioannis Karamanlakis <john.karamanlakis@gmail.com>
2014-12-17 00:02:05 +0000

    add uni email criteria for registration #34

--- courses/forms.py
+++ courses/forms.py
@@ -1,14 +1,32 @@
 from courses.models import CourseFeedback
 from django.contrib.auth.models import User
 from django import forms
+from django.core.validators import RegexValidator
 
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
+    email = forms.EmailField(validators=[
+        RegexValidator(
+            regex='ed.ac.uk$',
+            message='Email must belong to the ed.ac.uk domain',
+            code='invalid_email'
+        ), ]
+    )
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
+
+    # def is_valid(self):
+    #     valid = super(UserForm, self).is_valid()
+    #     if not valid:
+    #         return valid
+    #
+    #     if not self.fields['email'].endswith('@ed.ac.uk'):
+    #         return False
+    #
+    #     return True
 
 
 class CourseFeedbackForm(forms.ModelForm):
--- courses/views.py
+++ courses/views.py
@@ -9,10 +9,12 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
 
+# Home page
 def index(request):
     return render(request, 'courses/index.html')
 
 
+# Listing the Schools ordered by title
 class SchoolsIndexView(generic.ListView):
     template_name = 'courses/schools_index.html'
     model = School
@@ -22,6 +24,7 @@ class SchoolsIndexView(generic.ListView):
         return School.objects.order_by('title')
 
 
+# Displaying the courses that a school have
 def school_detail(request, school_id):
     school = get_object_or_404(School, pk=school_id)
     courses = school.course_set.order_by('title')
@@ -29,17 +32,21 @@ def school_detail(request, school_id):
     return render(request, 'courses/school_detail.html', context)
 
 
+# Displaying the feedback that a course has received
 def course_detail(request, course_id):
     course = get_object_or_404(Course, pk=course_id)
+    # visivle variable allow us not to display feedback that was inappropriate
     course_feedbacks = course.coursefeedback_set.filter(visible=True).order_by('-submission_date')
     context = {'course': course, 'course_feedbacks': course_feedbacks}
     return render(request, 'courses/course_detail.html', context)
 
 
+# Handles feedback given
 @login_required()
 def course_feedback(request, course_id):
     course = get_object_or_404(Course, pk=course_id)
     course_feedback_exists = False
+    # Check if this user has already submitted a review about the course, if yes she/he can edit it and resubmit
     old_feedback = CourseFeedback.objects.filter(user=request.user.id, course=course_id)
     if request.method == 'POST':
         if old_feedback:
@@ -55,6 +62,7 @@ def course_feedback(request, course_id):
         else:
             return HttpResponseBadRequest()
     else:
+        # if user has already submitted a feedback he/she can see it displayed
         if old_feedback:
             course_feedback_form = CourseFeedbackForm(instance=old_feedback.first())
             course_feedback_exists = True
@@ -65,27 +73,25 @@ def course_feedback(request, course_id):
         return render(request, 'courses/course_feedback.html', context)
 
 
+# Handles Registering process
 def user_register(request):
     if request.method == 'POST':
         user_form = UserForm(data=request.POST)
         if user_form.is_valid():
             user = user_form.save(commit=False)  # do not yet save in the DB until the password is encrypted
-            user.set_password(user.password)
+            user.set_password(user.password)    # password encryption
             user.save()
             return HttpResponseRedirect(reverse('courses:index'))
-        else:
-            return HttpResponseBadRequest()
     else:
         user_form = UserForm()
 
     context = {'user_form': user_form}
-    return render(request,
-                  'courses/register.html',
-                  context,
-    )
+    return render(request, 'courses/register.html', context)
 
 
+# Handles Login
 def user_login(request):
+    valid = True
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -101,7 +107,9 @@ def user_login(request):
             else:
                 return HttpResponse("Your unicoursebook account is disabled.")
         else:
-            return HttpResponse("Invalid login details supplied.")
+            valid = False
+            context = {'next_url': next_url, 'valid': valid}
+            return render(request, 'courses/login.html', context)
     else:
         # parameter 'next' contains the original url in case user tried to access a view that requires authentication
         # we need to propagate 'next' in the login form to redirect the user after login
@@ -109,12 +117,13 @@ def user_login(request):
 
         next_url = request.GET.get('next')
         if next_url:
-            context = {'next_url': next_url}
+            context = {'next_url': next_url, 'valid': valid}
         else:
             context = {}
         return render(request, 'courses/login.html', context)
 
 
+# Handles Logout
 def user_logout(request):
     logout(request)
     return HttpResponseRedirect(reverse('courses:index'))
